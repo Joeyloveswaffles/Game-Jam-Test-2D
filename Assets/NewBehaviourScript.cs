a812ff253cbc7d0fde47d118defc1a32e34e4ade
unknown <dthardi@ilstu.edu>
2020-01-15 22:38:24 -0600

    added a emo boi emote

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //;)
+        //;3
     }
 
     // Update is called once per frame
